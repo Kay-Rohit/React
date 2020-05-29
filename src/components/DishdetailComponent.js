55158875c02423e743118d526e5c1966f620f649
Kantimahanty Rohit <61279841+Kay-Rohit@users.noreply.github.com>
2020-05-29 18:48:12 +0530

    fetch from server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody,
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -110,7 +111,7 @@ render()
 function RenderDish(dishDetail) {
   return (
     <Card>
-      <CardImg top src={dishDetail.image} alt={dishDetail.name} />
+      <CardImg top src={baseUrl + dishDetail.image} alt={dishDetail.name} />
       <CardBody>
         <CardTitle>{dishDetail.name}</CardTitle>
         <CardText>{dishDetail.description}</CardText>
