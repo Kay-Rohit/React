58f93798f50f3b246b13182463b8f4fc0326b653
Kantimahanty Rohit <61279841+Kay-Rohit@users.noreply.github.com>
2020-05-30 11:26:06 +0530

    Error fetch problem resolved!!!

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -23,13 +23,13 @@ function RenderCard({ item, isLoading, errMess }) {
         transformProps={{ exitTransform: "scale(0.5) translateY(-50%)" }}
       >
         <Card>
-          <CardImg src={baseUrl + item.image} alt={item.name} />
+          <CardImg src={baseUrl + item?.image} alt={item?.name} />
           <CardBody>
-            <CardTitle>{item.name}</CardTitle>
-            {item.designation ? (
-              <CardSubtitle>{item.designation}</CardSubtitle>
+            <CardTitle>{item?.name}</CardTitle>
+            {item?.designation ? (
+              <CardSubtitle>{item?.designation}</CardSubtitle>
             ) : null}
-            <CardText>{item.description}</CardText>
+            <CardText>{item?.description}</CardText>
           </CardBody>
         </Card>
       </FadeTransform>
